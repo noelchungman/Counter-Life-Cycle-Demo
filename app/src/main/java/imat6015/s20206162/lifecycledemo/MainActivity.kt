86a4a2c5a6e7e4befe8f097652ab36a633edcb57
cm <65332417>
2023-04-24 21:09:17 +0800

    2nd Update

--- app/src/main/java/imat6015/s20206162/lifecycledemo/MainActivity.kt
+++ app/src/main/java/imat6015/s20206162/lifecycledemo/MainActivity.kt
@@ -10,40 +10,47 @@ class MainActivity : AppCompatActivity() {
 
     var counter = 0
 
+    // 7 functions, except new activity, 4 state of app only in fact
+    // New Activity -> Create + Start + Resume -> Running
+    // "Running" -> Pause ->  "Paused"
+    // "Paused" -> Resume ->"Running"
+    // "Paused" -> Stop -> "Stopped"
+    // "Stopped" -> Resume + Start + Restart -> "Running"
+    // "Stopped" -> Destroyed
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("LifeyCycle", "onCreate() is called")
+        Log.i("LifeCycle", "onCreate() is called")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i("LifeyCycle", "onPause() is called")
+        Log.i("LifeCycle", "onPause() is called")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i("LifeyCycle", "onResume() is called")
+        Log.i("LifeCycle", "onResume() is called")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i("LifeyCycle", "onStart() is called")
+        Log.i("LifeCycle", "onStart() is called")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("LifeyCycle", "onStop() is called")
+        Log.i("LifeCycle", "onStop() is called")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("LifeyCycle", "onDestroy() is called")
+        Log.i("LifeCycle", "onDestroy() is called")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i("LifeyCycle", "onRestart() is called")
+        Log.i("LifeCycle", "onRestart() is called")
     }
 
     fun increaseCounter(v: View) {
